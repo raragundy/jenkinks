9dde011d7a4ac3137e8aebb0b075eb7dec8ed434
Rudy Aragundy <rudy.aragundi@gmail.com>
2022-09-02 14:49:46 -0500

    Changes to be committed: 	new file:   app.py 	new file:   requirements.txt 	new file:   test_app.py

---
+++ test_app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os
+import tempfile
+
+import pytest
+
+from app import app
+
+@pytest.fixture
+
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    
+    
+    yield client
+    
+def test_valid_transaction(client):
+    card = {
+        "status": True,
+        "number": 123456,
+        "limit": 10000,
+        "transaction":{
+             "amount":500,
+            }
+        }
+    
+    rv = client.post("/api/transaction", json=card)
+    assert True == rv.get_json().get("approved")
+    assert 500 == rv.get_json().get("newLimit")
+    
+    
+def test_above_limit(client):
+     card = {
+        "status": False,
+        "number": 123456,
+        "limit": 10000,
+        "transaction":{
+             "amount":500,
+            }
+        }
+     
+     rv = client.post("/api/transaction", json=card)
+     assert False == rv.get_json().get("approved")
+     assert "Transaction above the limit" in rv.get_json().get("reason")
+     
+def test_above_limit(client):
+     card = {
+        "status": False,
+        "number": 123456,
+        "limit": 10000,
+        "transaction":{
+             "amount":500,
+            }
+        }
+     
+     rv = client.post("/api/transaction", json=card)
+     assert False == rv.get_json().get("approved")
+     assert "Blocked Card" in rv.get_json().get("reason")
+    
+
